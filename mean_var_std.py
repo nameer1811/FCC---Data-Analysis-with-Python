939ab7fdec55f82e057ef7d19ff06fd2cc7a012c
Sheikh Fahad <70671788+nameer1811@users.noreply.github.com>
2021-10-16 23:25:19 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,27 +1,25 @@
+#mean_var_std
 import numpy as np
+a = [2,6,2,8,4,0,1,5,7]
 
-def calculate(list):
-  if len(list) != 9:
-    raise ValueError("List must contain nine numbers.")
-  
-  listToArray = np.array(list)
-  array3d = listToArray.reshape((3,3))
+listToArray = np.array(a)
+array3d = listToArray.reshape((3,3))
 
-  meanList = [array3d.mean(axis=0).tolist(), array3d.mean(axis = 1).tolist(), array3d.mean()]
-  varianceList = [array3d.var(axis=0).tolist(),array3d.var(axis=1).tolist(), array3d.var()]
-  standardDevList = [array3d.std(axis=0).tolist(),array3d.std(axis=1).tolist(), array3d.std()]
-  maxList = [array3d.max(axis=0).tolist(), array3d.max(axis=1).tolist(), array3d.max()]
-  minList = [array3d.min(axis=0).tolist(), array3d.min(axis=1).tolist(), array3d.min()]
-  sumList = [array3d.sum(axis=0).tolist(), array3d.sum(axis=1).tolist(), array3d.sum()]
+meanList = [array3d.mean(axis=0).tolist(), array3d.mean(axis = 1).tolist(), array3d.mean().tolist()]
+varianceList = [array3d.var(axis=0).tolist(),array3d.var(axis=1).tolist(), array3d.var().tolist()]
+standardDevList = [array3d.std(axis=0).tolist(),array3d.std(axis=1).tolist(), array3d.std().tolist()]
+maxList = [array3d.max(axis=0).tolist(), array3d.max(axis=1).tolist(), array3d.max().tolist()]
+minList = [array3d.min(axis=0).tolist(), array3d.min(axis=1).tolist(), array3d.min().tolist()]
+sumList = [array3d.sum(axis=0).tolist(), array3d.sum(axis=1).tolist(), array3d.sum().tolist()]
 
 
-  calculations = {}
+calculations = {}
 
-  calculations['mean'] = meanList
-  calculations['variance'] = varianceList
-  calculations['standard deviation'] = standardDevList
-  calculations['max'] = maxList
-  calculations['min'] = minList
-  calculations['sum'] = sumList
-    
-  return calculations
+calculations['mean'] = meanList
+calculations['variance'] = varianceList
+calculations['standard deviation'] = standardDevList
+calculations['max'] = maxList
+calculations['min'] = minList
+calculations['sum'] = sumList
+
+print(calculations)
